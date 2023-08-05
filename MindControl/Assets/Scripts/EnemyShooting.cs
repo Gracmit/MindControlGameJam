2acfb0446fdcb777a942f0ede57adb43d7f08376
Gracmit <aleksii97@gmail.com>
2023-08-05 11:55:11 +0300

    Enemy Shooting abitlity. Still a bit buggy

---
+++ MindControl/Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject _cannon;
+    [SerializeField] private GameObject _cannonLegs;
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _bulletPoint;
+
+    private GameObject _player;
+    private float _shootTimer = 0f;
+    private float _shootingDelay;
+    private void Awake()
+    {
+        _player = FindObjectOfType<Shooting>().gameObject;
+        
+    }
+
+    void Update()
+    {
+        RotateCannon();
+        if(_shootTimer < Time.time)
+            Shoot();
+    }
+
+    private void Shoot()
+    {
+        _shootTimer = Time.time + _shootingDelay;
+        var instantiatedObject = Instantiate(_bullet, _bulletPoint.position, _cannon.transform.rotation);
+        var bullet = instantiatedObject.GetComponent<Bullet>();
+        bullet.Shoot();
+    }
+
+    private void RotateCannon()
+    {
+        _cannonLegs.transform.LookAt(_player.transform.position);
+        _cannonLegs.transform.Rotate(_cannonLegs.transform.up, 90);
+        _cannonLegs.transform.localEulerAngles = new Vector3(0, _cannonLegs.transform.eulerAngles.y, 0);
+        _cannon.transform.LookAt(_player.transform.position);
+        _cannon.transform.Rotate(_cannon.transform.up, 90);
+        _cannon.transform.localEulerAngles = new Vector3(0, 0, _cannon.transform.eulerAngles.z);
+    }
+}
