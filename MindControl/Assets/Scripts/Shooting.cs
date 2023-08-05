c47b0c0833190cda9a83f3bca3dc0ca2c0494510
Gracmit <aleksii97@gmail.com>
2023-08-05 10:27:04 +0300

    Added ammo count and the shooting reacts to it accordingly.

--- MindControl/Assets/Scripts/Shooting.cs
+++ MindControl/Assets/Scripts/Shooting.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
@@ -6,6 +8,13 @@ public class Shooting : MonoBehaviour
     [SerializeField] private GameObject _bullet;
     [SerializeField] private Transform _shootingPoint;
     [SerializeField] private int _ammoCount;
+    [SerializeField] private TMP_Text _ammoText;
+
+
+    private void Start()
+    {
+        UpdateAmmoText();
+    }
 
     void Update()
     {
@@ -16,6 +25,13 @@ public class Shooting : MonoBehaviour
             
             var bullet = instantiatedObject.GetComponent<Bullet>();
             bullet.Shoot();
+            _ammoCount--;
+            UpdateAmmoText();
         }
+    }
+
+    public void UpdateAmmoText()
+    {
+        _ammoText.text = $"Amm0: {_ammoCount}";
     }
 }
