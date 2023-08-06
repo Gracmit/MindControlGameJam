f271149f99b52110f24170cff224a1ed60056089
Gracmit <aleksii97@gmail.com>
2023-08-06 11:50:05 +0300

    Added cooldown and sfx to ammo pickup

--- MindControl/Assets/Scripts/AmmoBox.cs
+++ MindControl/Assets/Scripts/AmmoBox.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 public class AmmoBox : MonoBehaviour
 {
     [SerializeField] private int _ammoAmount;
+    [SerializeField] private GameObject _bullets;
+    private BoxCollider _collider;
+    private AudioSource _audio;
+    private float cdTimer = 0;
+    private float cdTime = 10;
+
+    private void Awake()
+    {
+        _collider = GetComponent<BoxCollider>();
+        _audio = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (cdTimer < Time.time && _collider.enabled == false)
+        {
+            _collider.enabled = true;
+            _bullets.SetActive(true);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            _audio.Play();
             var shooting = other.gameObject.GetComponent<Shooting>();
             shooting.AddAmmo(_ammoAmount);
+            _collider.enabled = false;
+            _bullets.SetActive(false);
+            cdTimer = Time.time + cdTime;
         }
     }
 }
