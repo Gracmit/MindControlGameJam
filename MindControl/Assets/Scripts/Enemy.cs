1d208299c5180cdeab502c9e35f7465611fb2e50
Gracmit <aleksii97@gmail.com>
2023-08-06 13:34:39 +0300

    Enemy healthbar

--- MindControl/Assets/Scripts/Enemy.cs
+++ MindControl/Assets/Scripts/Enemy.cs
@@ -1,19 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _hp;
     [SerializeField] private GameObject _healthPickUp;
     [SerializeField] private AudioSource _audioHit;
+    [SerializeField] private Slider _slider;
+
+    private void Awake()
+    {
+        _slider.value = _hp;
+    }
 
     public void TakeHit(int amount)
     {
+        _slider.value = _hp;
         _audioHit.Play();
         _hp -= amount;
         if (_hp < 0)
         {
             _hp = 0;
-
+            _slider.value = _hp;
             Die();
         }
     }
